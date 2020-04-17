8d2b3f07b20ab857355644729508044b409acaa5
Daniel da Silva <daniel@developmentseed.org>
2020-04-17 11:49:11 +0100

    Update OSRM profile to fit Haiti use case

--- instance/osrm_profile-haiti.lua
+++ instance/osrm_profile-haiti.lua
@@ -1,203 +1,149 @@
--- Car profile
+-- ## OSRM profile file
+
+-- The profile file is used to define speeds and routability for OSRM. It is
+-- possible to define speeds conditionally based on way tags and other attributes.
+
+-- The profile used for this project is pretty simple and straightforward.
+-- The road network only contains roads that we need to account for, not having
+-- one ways, traffic lights, private roads, etc. All roads are considered routable
+-- depending on the condition and surface.
+
+
+-- Additional information about `.lua` profiles:
+-- - https://www.winwaed.com/blog/2015/11/18/osrms-lua-scripts/
+-- - https://github.com/Project-OSRM/osrm-backend/wiki/Profiles
+-- - https://github.com/Project-OSRM/osrm-backend/blob/master/docs/profiles.md
+
+-- NOTE:
+-- For some reason osrm-customize was breaking
+-- The process won't error but just print:
+
+-- $ docker run -t -v "${PWD}:/data" osrm/osrm-backend osrm-customize /data/road-network.osrm
+-- [info] Loaded edge based graph: 434864 edges, 105748 nodes
+-- [info] Loading partition data took 0.240434 seconds
+
+-- Turns out it was the value of weight_precision. If it is to high, osrm is
+-- not able to store it.
 
 api_version = 4
 
-Set = require('lib/set')
-Sequence = require('lib/sequence')
-Handlers = require("lib/way_handlers")
-Relations = require("lib/relations")
-limit = require("lib/maxspeed").limit
-Utils = require("lib/utils")
-Measure = require("lib/measure")
-
-function setup()
+-- Define the properties and configuration.
+function setup ()
   return {
     properties = {
-      max_speed_for_map_matching      = 180/3.6, -- 180kmph -> m/s
-      -- For routing based on duration, but weighted for preferring certain roads
-      weight_name                     = 'routability',
-      -- For shortest duration without penalties for accessibility
-      -- weight_name                     = 'duration',
-      -- For shortest distance without penalties for accessibility
-      -- weight_name                     = 'distance',
-      process_call_tagless_node      = false,
-      u_turn_penalty                 = 20,
-      continue_straight_at_waypoint  = true
+      -- Increase accuracy of routing.
+      max_speed_for_map_matching      = 180/3.6, -- kmph -> m/s
+      weight_name                     = 'duration',
+      weight_precision                = 1
     },
 
-    default_mode              = mode.driving,
-    default_speed             = 10,
-    side_road_multiplier      = 0.8,
-    turn_penalty              = 7.5,
-    speed_reduction           = 0.8,
-    turn_bias                 = 1.075,
-
-    speeds = Sequence {
-      type = {
-        RI              = 90,
-        RN              = 65,
-        RD              = 55,
-        RC              = 30,
-        RA              = 25,
-        RU              = 20
-      }
+    speeds_type = {
+      RN = 100,
+      RD = 90,
+      RC = 80,
+      RA = 70,
+      RU = 60,
+      RI = 50
     },
 
-    service_penalties = {
+    penalties_seasonality = {
+      ["all-weather"] = 0,
+      ["dry-weather"] = 20
+      -- "non-passable" = BLOCK 
     },
 
-    restricted_highway_whitelist = Set {
+    penalties_width = {
+      large = 0,
+      medium = 10,
+      small = 20
     },
 
-    construction_whitelist = Set {
-    },
-
-    route_speeds = {
-    },
-
-    bridge_speeds = {
-    },
-
-    access_tags_hierarchy = Sequence {
-    },
-
-    -- surface/trackype/smoothness
-    -- values were estimated from looking at the photos at the relevant wiki pages
-
-    -- max speed for surfaces
-    surface_speeds = {
-      asphalt = nil,    -- nil mean no limit. removing the line has the same effect
-      concrete = nil,
-      ["concrete:plates"] = nil,
-      ["concrete:lanes"] = nil,
-      paved = nil,
-
-      cement = 80,
-      compacted = 80,
-      fine_gravel = 80,
-
-      paving_stones = 60,
-      metal = 60,
-      bricks = 60,
-
-      grass = 40,
-      wood = 40,
-      sett = 40,
-      grass_paver = 40,
-      gravel = 40,
-      unpaved = 40,
-      ground = 40,
-      dirt = 40,
-      pebblestone = 40,
-      tartan = 40,
-
-      cobblestone = 30,
-      clay = 30,
-
-      earth = 20,
-      stone = 20,
-      rocky = 20,
-      sand = 20,
-
-      mud = 10
-    },
-
-    -- max speed for tracktypes
-    tracktype_speeds = {
-      grade1 =  60,
-      grade2 =  40,
-      grade3 =  30,
-      grade4 =  25,
-      grade5 =  20
-    },
-
-    -- max speed for smoothnesses
-    smoothness_speeds = {
-      intermediate    =  80,
-      bad             =  40,
-      very_bad        =  20,
-      horrible        =  10,
-      very_horrible   =  5,
-      impassable      =  0
-    },
-
-    -- http://wiki.openstreetmap.org/wiki/Speed_limits
-    maxspeed_table_default = {
-      urban = 50,
-      rural = 90,
-      trunk = 110,
-      motorway = 130
-    },
-
-    relation_types = Sequence {
-    },
-
-    -- classify highway tags when necessary for turn weights
-    highway_turn_classification = {
-    },
-
-    -- classify access tags when necessary for turn weights
-    access_turn_classification = {
+    penalties_surface = {
+      ["asphalt"] = 0,
+      ["stabilized-soil"] = 20,
+      ["earth"] = 40
     }
   }
 end
 
-function process_node(profile, node, result, relations)
+function process_node (profile, node, result)
   -- The road network is very simple. Nothing to do on nodes.
 end
 
-function process_way(profile, way, result, relations)
-  -- the intial filtering of ways based on presence of tags
-  -- affects processing times significantly, because all ways
-  -- have to be checked.
-  -- to increase performance, prefetching and intial tag check
-  -- is done in directly instead of via a handler.
-
-  -- in general we should  try to abort as soon as
-  -- possible if the way is not routable, to avoid doing
-  -- unnecessary work. this implies we should check things that
-  -- commonly forbids access early, and handle edge cases later.
-
-  -- data table for storing intermediate values during processing
-  local data = {
-    -- prefetch tags
-    type = way:get_value_by_key('type'),
-    bridge = way:get_value_by_key('bridge')
-  }
-
-  -- perform an quick initial check and abort if the way is
-  -- obviously not routable.
-  -- type tags must be in data table, bridge is optional
-  if (not data.type)
-  then
+function process_way (profile, way, result)
+  -- Check if it is not passable.
+  -- local seasonality = way:get_value_by_key('seasonality')
+  if seasonality == 'non-passable' then
     return
   end
 
-  handlers = Sequence {
-    -- set the default mode for this profile. if can be changed later
-    -- in case it turns we're e.g. on a ferry
-    WayHandlers.default_mode,
+  -- Use the way id tag as its name
+  local name = way:get_value_by_key('id')
+  -- Set the name that will be used for instructions
+  if name then
+    result.name = name
+  end
 
-    -- compute speed taking into account way type, maxspeed tags, etc.
-    WayHandlers.speed,
-    WayHandlers.surface,
-    WayHandlers.maxspeed,
-    WayHandlers.penalties,
+  result.forward_mode = mode.driving
+  result.backward_mode = mode.driving
 
-    -- compute class labels
-    WayHandlers.classes,
-  }
+  local waySpeed = nil
 
-  WayHandlers.run(profile, way, result, data, handlers, relations)
+  -- -- Type
+  local type = way:get_value_by_key('type')
+  local typeSpeed = profile.speeds_type[type]
 
+  if not typeSpeed then
+    waySpeed = profile.speeds_type.RI
+  else
+    waySpeed = typeSpeed
+  end
+
+  -- -- Seasonality
+  local seasonalityPen = profile.penalties_seasonality[seasonality]
+
+  if seasonalityPen or seasonalityPen == 0 then
+    waySpeed = waySpeed - seasonalityPen
+  else
+    waySpeed = waySpeed - profile.penalties_seasonality["dry-weather"]
+  end
+
+  -- -- Surface
+  local surface = way:get_value_by_key('surface')
+  local surfacePen = profile.penalties_surface[surface]
+
+  if surfacePen or surfacePen == 0 then
+    waySpeed = waySpeed - surfacePen
+  else
+    waySpeed = waySpeed - profile.penalties_surface.earth
+  end
+
+  -- -- Width
+  local width = way:get_value_by_key('width')
+  local widthPen = profile.penalties_width[width]
+
+  if widthPen or widthPen == 0 then
+    waySpeed = waySpeed - widthPen
+  else
+    waySpeed = waySpeed - profile.penalties_width.small
+  end
+
+  waySpeed = math.max(waySpeed, 10)
+
+  result.forward_speed = waySpeed
+  result.backward_speed = waySpeed
+
+  result.forward_rate = waySpeed / 3.6
+  result.backward_rate = waySpeed / 3.6
 end
 
-function process_turn(profile, turn)
-  -- The road network is very simple. Nothing to do on turns.
+function process_turn (profile, turn)
+  -- There are no turn restrictions to process.
 end
 
 return {
   setup = setup,
   process_way = process_way,
-  process_node = process_node
+  process_node = process_node,
+  process_turn = process_turn
 }
