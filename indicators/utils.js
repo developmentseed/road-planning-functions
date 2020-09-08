2deb9d99b67a4a39c872e5e1d1638b389da9b556
Olaf Veerman <olaf.veerman@gmail.com>
2020-09-08 11:19:15 +0100

    Add option to generate scaled score using logscale

--- indicators/utils.js
+++ indicators/utils.js
@@ -3,22 +3,43 @@ function round (value, decimals = 2) {
 }
 
 /**
- * Adds a scaled score (0-100) to objects
+ * Adds a scaled score to objects (0 - 100). By default it applies a linear
+ * scale, though logscale is also available.
  *
- * @param  {Array} data        Data to add the score to.
+ * @param {Array}   data              Data to add the score to.
+ * @param {Number}  data[].value      The value to scale.
+ * @param {Object}  [options]         Options object.
+ * @param {Boolean} [options.log]     Use logscale instead of linear.
  *
  * @example
  * // returns [{'value': 20, 'score': 40}, {'value': 50, 'score': 100}]
  * addScaledScore([{'value': 20}, {'value': 50}])
  *
- * @return {Array}             Array with scaled
+ * @return {Array}             Array with scaled score.
  *
  */
-function addScaledScore (data) {
-  const maxValue = Math.max(...data.map(w => w.value).filter(v => !isNaN(v)));
+function addScaledScore (data, options = {}) {
+  const {
+    log = false
+  } = options;
+
+  const values = data.map(datum => datum.value).filter(value => !isNaN(value));
+
+  const domain = log
+    ? [Math.log(Math.min(...values)), Math.log(Math.max(...values))]
+    : [Math.min(...values), Math.max(...values)];
+  const range = domain[1] - domain[0];
 
   // Scale values from 0-100
-  return data.map(datum => ({ ...datum, score: round(datum.value / maxValue * 100, 2) }));
+  return data
+    .filter(datum => !isNaN(datum.value))
+    .map(datum => {
+      const value = log ? Math.log(datum.value) : datum.value;
+      return {
+        ...datum,
+        score: round((value - domain[0]) * 100 / range, 2)
+      };
+    });
 }
 
 module.exports = {
--- tests/indicators.test.js
+++ tests/indicators.test.js
@@ -3,16 +3,43 @@ const { assert } = require('chai');
 const { addScaledScore } = require('../indicators/utils');
 
 describe('Add scaled score to indicators', function () {
-  it('Add correct score', function () {
+  it('Add correct score with default options', function () {
     const input = [
-      { id: 1, value: 60 },
+      { id: 1, value: 70 },
       { id: 2, value: 20 },
       { id: 3, value: 120 }
     ];
     const output = [
-      { id: 1, value: 60, score: 50 },
-      { id: 2, value: 20, score: 16.67 },
+      { id: 1, value: 70, score: 50 },
+      { id: 2, value: 20, score: 0 },
       { id: 3, value: 120, score: 100 }
+    ];
+    assert.deepEqual(addScaledScore(input), output);
+  });
+
+  it('Add correct score with logscale', function () {
+    const input = [
+      { value: 70 },
+      { value: 20 },
+      { value: 10000 }
+    ];
+    const output = [
+      { value: 70, score: 20.16 },
+      { value: 20, score: 0 },
+      { value: 10000, score: 100 }
+    ];
+    assert.deepEqual(addScaledScore(input, { log: true }), output);
+  });
+
+  it('Handles non-numeric values', function () {
+    const input = [
+      { value: 0 },
+      { value: 'AmIANumber?' },
+      { value: 100 }
+    ];
+    const output = [
+      { value: 0, score: 0 },
+      { value: 100, score: 100 }
     ];
     assert.deepEqual(addScaledScore(input), output);
   });
